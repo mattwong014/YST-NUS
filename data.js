af36eb9a5f7c590142f0b539a78063bb8964ad41
mattwong014 <94966035+mattwong014@users.noreply.github.com>
2021-12-16 07:36:00 +0000

    First commit

---
+++ data.js
@@ -0,0 +1,139 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-alice-lee-plaza",
+      "name": "Alice Lee Plaza",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -3.044249269238355,
+        "pitch": -0.10153368896232173,
+        "fov": 1.4210867929104314
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.062376894184762,
+          "pitch": 0.05511203058356173,
+          "rotation": 0,
+          "target": "1-conservatory-foyer"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.4476292944795084,
+          "pitch": 0.03705635010427244,
+          "title": "Alice Lee Plaza",
+          "text": "This is an outdoor plaza where students can meet and hang out."
+        }
+      ]
+    },
+    {
+      "id": "1-conservatory-foyer",
+      "name": "Conservatory Foyer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9485424738983728,
+          "pitch": 0.055193547210093286,
+          "rotation": 0,
+          "target": "2-conservatory-lounge"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.6327737349435507,
+          "pitch": 0.07400513182865431,
+          "title": "Conservatory Foyer",
+          "text": "This is the Foyer for the School of Music's Conservatory."
+        }
+      ]
+    },
+    {
+      "id": "2-conservatory-lounge",
+      "name": "Conservatory Lounge",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 2.741452828463098,
+          "pitch": 0.09174916466731275,
+          "title": "Conservatory Lounge",
+          "text": "The Lounge is a spot where students can mix and congregate. There is also a piano which is free for any student to use.&nbsp;"
+        }
+      ]
+    }
+  ],
+  "name": "YST-Music",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
